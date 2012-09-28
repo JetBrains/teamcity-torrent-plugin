bc5b9f52ae5228c49d0be7418c96feea76756ca0
Pavel.Sher <devnull@localhost>
2012-09-28 21:17:53 +0200

    working on configuration

--- mirror-service/src/jetbrains/buildServer/artifactsMirror/server/ServerMain.java
+++ mirror-service/src/jetbrains/buildServer/artifactsMirror/server/ServerMain.java
@@ -1,7 +1,33 @@
 package jetbrains.buildServer.artifactsMirror.server;
 
+import java.io.File;
+import java.io.IOException;
+
 public class ServerMain {
   public static void main(String[] args) {
+    if (args.length == 0) {
+      usage();
+    }
 
+    String confFile = args[0];
+
+    try {
+      new ServerConfiguration(new File(confFile));
+    } catch (IOException e) {
+      System.err.println("Input/output error accessing properties file: " + confFile + ", error: " + e.toString());
+      exit();
+    } catch (InvalidConfigurationException e) {
+      System.err.println("Configuration specified in properties file is invalid: " + e.toString());
+      exit();
+    }
+  }
+
+  private static void usage() {
+    System.out.println("Usage: java -jar mirror-server.jar <path to properties file>");
+    exit();
+  }
+
+  private static void exit() {
+    System.exit(1);
   }
 }
