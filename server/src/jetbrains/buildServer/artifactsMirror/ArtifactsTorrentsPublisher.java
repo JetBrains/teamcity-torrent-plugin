6e603150157ff904eb8254f89d8e9c35ce31f071
maxim <smartmaxim@gmail.com>
2012-10-01 14:21:39 +0200

    ArtifactsTorrentsPublisher: initial

---
+++ server/src/jetbrains/buildServer/artifactsMirror/ArtifactsTorrentsPublisher.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2000-2012 by JetBrains s.r.o. All Rights Reserved.
+ * Use is subject to license terms.
+ */
+package jetbrains.buildServer.artifactsMirror;
+
+import jetbrains.buildServer.artifactsMirror.torrent.TorrentTracker;
+import jetbrains.buildServer.serverSide.BuildServerAdapter;
+import jetbrains.buildServer.serverSide.SBuildServer;
+import jetbrains.buildServer.serverSide.SRunningBuild;
+import jetbrains.buildServer.serverSide.artifacts.ArtifactsGuard;
+import jetbrains.buildServer.serverSide.artifacts.BuildArtifact;
+import jetbrains.buildServer.serverSide.artifacts.BuildArtifacts;
+import jetbrains.buildServer.serverSide.artifacts.BuildArtifactsViewMode;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+/**
+ * @author Maxim Podkolzine (maxim.podkolzine@jetbrains.com)
+ * @since 8.0
+ */
+public class ArtifactsTorrentsPublisher extends BuildServerAdapter {
+  private static final int MIN_ARTIFACT_SIZE = 10 * (1 << 20);              // 10 Mb
+
+  private final ArtifactsGuard myGuard;
+  private final TorrentTracker myTorrentTracker;
+
+  public ArtifactsTorrentsPublisher(@NotNull SBuildServer buildServer,
+                                    @NotNull ArtifactsGuard guard,
+                                    @NotNull TorrentTracker torrentTracker) {
+    myGuard = guard;
+    myTorrentTracker = torrentTracker;
+    buildServer.addListener(this);
+  }
+
+  @Override
+  public void buildFinished(SRunningBuild build) {
+    final File artifactsDirectory = build.getArtifactsDirectory();
+
+    BuildArtifacts artifacts = build.getArtifacts(BuildArtifactsViewMode.VIEW_DEFAULT);
+    artifacts.iterateArtifacts(new BuildArtifacts.BuildArtifactsProcessor() {
+      @NotNull
+      public Continuation processBuildArtifact(@NotNull BuildArtifact artifact) {
+        if (shouldCreateTorrentFor(artifact)) {
+          File artifactFile = new File(artifactsDirectory, artifact.getRelativePath());
+          myGuard.lockReading(artifactFile);
+          try {
+            myTorrentTracker.announceTorrent(artifactFile);
+          } finally {
+            myGuard.unlockReading(artifactFile);
+          }
+        }
+        return Continuation.CONTINUE;
+      }
+    });
+  }
+
+  private static boolean shouldCreateTorrentFor(@NotNull BuildArtifact artifact) {
+    long size = artifact.getSize();
+    return size > MIN_ARTIFACT_SIZE;
+  }
+}
