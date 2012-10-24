e38d377ea4eabde62eda88abf4d12c03837188a4
maxim <smartmaxim@gmail.com>
2012-10-24 11:30:22 +0200

    Improve the code to get rid of "torrentIcon" in the URL

--- server/src/jetbrains/buildServer/artifactsMirror/web/TorrentLinksExtension.java
+++ server/src/jetbrains/buildServer/artifactsMirror/web/TorrentLinksExtension.java
@@ -10,27 +10,15 @@ import jetbrains.buildServer.web.openapi.PluginDescriptor;
 import jetbrains.buildServer.web.openapi.SimplePageExtension;
 import org.jetbrains.annotations.NotNull;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
-
 /**
  * @author Maxim Podkolzine (maxim.podkolzine@jetbrains.com)
  * @since 8.0
  */
 public class TorrentLinksExtension extends SimplePageExtension {
-  private final PluginDescriptor myDescriptor;
-
   public TorrentLinksExtension(@NotNull PagePlaces pagePlaces,
                                @NotNull PluginDescriptor descriptor) {
     super(pagePlaces, PlaceId.ALL_PAGES_FOOTER, "torrent-links", descriptor.getPluginResourcesPath("torrentLinks.jsp"));
-    myDescriptor = descriptor;
     addJsFile(descriptor.getPluginResourcesPath("torrentLinks.js"));
     register();
-  }
-
-  @Override
-  public void fillModel(@NotNull Map<String, Object> model, @NotNull HttpServletRequest request) {
-    super.fillModel(model, request);
-    model.put("torrentIcon", myDescriptor.getPluginResourcesPath("torrent.png"));
   }
 }
