03af05001a4f8b37ab79e6457f8a12754972c261
Sergey.Pak <sergey.pak@jetbrains.com>
2013-11-02 17:29:26 +0400

    TW-33165 and fixed FileLinkTest.testWindowsLink on linux agents

--- common/tests/java/jetbrains/buildServer/torrent/seeder/FileLinkTest.java
+++ common/tests/java/jetbrains/buildServer/torrent/seeder/FileLinkTest.java
@@ -19,8 +19,8 @@ public class FileLinkTest extends BaseTestCase {
     final File targetFile = FileLink.getTargetFile(linkFile);
     final File torrentFile = FileLink.getTorrentFile(linkFile);
 
-    assertEquals(new File("artifacts/MyTestOne/4/93/sampleDir/art3.33.dat"), targetFile);
-    assertEquals(new File("artifacts/MyTestOne/4/93/.teamcity/torrents/sampleDir/art3.33.dat.torrent"), torrentFile);
+    assertEquals(new File("artifacts\\MyTestOne\\4\\93\\sampleDir\\art3.33.dat"), targetFile);
+    assertEquals(new File("artifacts\\MyTestOne\\4\\93\\.teamcity\\torrents\\sampleDir\\art3.33.dat.torrent"), torrentFile);
   }
 
   public void testLinuxLink() throws IOException {
