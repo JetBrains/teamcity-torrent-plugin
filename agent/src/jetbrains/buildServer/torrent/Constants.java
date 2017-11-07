0f49b07029a0b9fcffc89c1caeeb14c7a8f0c669
Maxim Zaytsev <Maxim.Zaytsev@jetbrains.com>
2017-11-07 14:15:22 +0300

    changed creating cache static dirs: switched String.format to just string concatenation for better visibility

--- agent/src/jetbrains/buildServer/torrent/Constants.java
+++ agent/src/jetbrains/buildServer/torrent/Constants.java
@@ -23,7 +23,7 @@ import java.io.File;
 public class Constants {
   public static final String TORRENTS_DIRNAME = "torrents";
   public static final String TORRENTS_DIR_ON_SERVER = ArtifactsConstants.TEAMCITY_ARTIFACTS_DIR + "/" + TORRENTS_DIRNAME + "/";
-  public static final String CACHE_STATIC_DIRS = String.format("httpAuth%1$srepository%1$sdownload", File.separator);
+  public static final String CACHE_STATIC_DIRS = "httpAuth" + File.separator + "repository" + File.separator + "download";
   public static final String TEAMCITY_IVY = "teamcity-ivy.xml";
   public static final String TORRENT_FILE_COPIES_DIR = "tempTorrentFilesCopies";
 }
