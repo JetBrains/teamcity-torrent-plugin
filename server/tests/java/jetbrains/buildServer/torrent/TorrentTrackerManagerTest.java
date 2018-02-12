385dc30bab966643b863b7b7f91518bdd5de2189
Maxim Zaytsev <Maxim.Zaytsev@jetbrains.com>
2018-02-12 16:13:03 +0300

    added tests for getConnectedClientsNum method

---
+++ server/tests/java/jetbrains/buildServer/torrent/TorrentTrackerManagerTest.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2000-2018 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.buildServer.torrent;
+
+import com.turn.ttorrent.common.PeerUID;
+import com.turn.ttorrent.tracker.AddressChecker;
+import com.turn.ttorrent.tracker.TrackedPeer;
+import com.turn.ttorrent.tracker.TrackedTorrent;
+import jetbrains.buildServer.RootUrlHolder;
+import jetbrains.buildServer.XmlRpcHandlerManager;
+import jetbrains.buildServer.serverSide.BuildServerListener;
+import jetbrains.buildServer.serverSide.ServerPaths;
+import jetbrains.buildServer.serverSide.executors.ExecutorServices;
+import jetbrains.buildServer.util.EventDispatcher;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.testng.Assert.assertEquals;
+
+public class TorrentTrackerManagerTest {
+
+  private ExecutorService myExecutorService;
+  private TorrentTrackerManager myTorrentTrackerManager;
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+
+    Mockery m = new Mockery();
+    final XmlRpcHandlerManager rpcHandlerManager = m.mock(XmlRpcHandlerManager.class);
+    final ExecutorServices executorServices = m.mock(ExecutorServices.class);
+    myExecutorService = Executors.newScheduledThreadPool(4);
+    final RootUrlHolder rootUrlHolder = m.mock(RootUrlHolder.class);
+    m.checking(new Expectations() {{
+      allowing(rpcHandlerManager).addHandler(with(any(String.class)), with(any(Object.class)));
+      allowing(executorServices).getNormalExecutorService(); will(returnValue(myExecutorService));
+      allowing(rootUrlHolder).getRootUrl(); will(returnValue("http://localhost:8111"));
+    }});
+    myTorrentTrackerManager = new TorrentTrackerManager(
+            new TorrentConfigurator(new ServerPaths("/tmp"), rootUrlHolder, rpcHandlerManager),
+            executorServices,
+            new EventDispatcher<BuildServerListener>(BuildServerListener.class) {
+            },
+            m.mock(AddressChecker.class)
+    );
+
+  }
+
+  @AfterMethod
+  public void tearDown() throws Exception {
+    myExecutorService.shutdown();
+  }
+
+  @Test
+  public void testThatOnePeerWithManyTorrentsCalculatedAsOnePeer() {
+    TrackedTorrent firstTorrent = new TrackedTorrent(new byte[]{1});
+    TrackedTorrent secondTorrent = new TrackedTorrent(new byte[]{2});
+
+    myTorrentTrackerManager.startTracker();
+
+    final String ip = "127.0.0.1";
+    final int port = 6881;
+    firstTorrent.getPeers().put(new PeerUID(new InetSocketAddress(ip, port), "1"),
+            new TrackedPeer(firstTorrent, ip, port, ByteBuffer.allocate(10)));
+    firstTorrent.getPeers().put(new PeerUID(new InetSocketAddress(ip, port), "2"),
+            new TrackedPeer(secondTorrent, ip, port, ByteBuffer.allocate(10)));
+    myTorrentTrackerManager.getTorrents().putIfAbsent("1", firstTorrent);
+    myTorrentTrackerManager.getTorrents().putIfAbsent("2", secondTorrent);
+
+    assertEquals(2, myTorrentTrackerManager.getTorrents().size());
+    assertEquals(1, myTorrentTrackerManager.getConnectedClientsNum());
+
+    firstTorrent.getPeers().put(new PeerUID(new InetSocketAddress(ip, port + 1), "1"),
+            new TrackedPeer(firstTorrent, ip, port + 1, ByteBuffer.allocate(10)));
+
+    assertEquals(2, myTorrentTrackerManager.getTorrents().size());
+    assertEquals(2, myTorrentTrackerManager.getConnectedClientsNum());
+
+  }
+}
