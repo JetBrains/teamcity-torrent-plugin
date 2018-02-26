8c5a0c10c0359e2293ef3b5bc1a38de79a595706
Maxim Zaytsev <Maxim.Zaytsev@jetbrains.com>
2018-02-26 17:00:42 +0300

    added providing agent parameter from torrent settings into new builds

---
+++ server/src/jetbrains/buildServer/torrent/BuildStartListener.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2000-2018 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.buildServer.torrent;
+
+import jetbrains.buildServer.serverSide.BuildStartContext;
+import jetbrains.buildServer.serverSide.BuildStartContextProcessor;
+import jetbrains.buildServer.torrent.settings.LeechSettings;
+import jetbrains.buildServer.torrent.settings.SeedSettings;
+import org.jetbrains.annotations.NotNull;
+
+public class BuildStartListener implements BuildStartContextProcessor {
+
+  private final TorrentConfigurator myConfigurator;
+
+  public BuildStartListener(TorrentConfigurator configurator) {
+    myConfigurator = configurator;
+  }
+
+  @Override
+  public void updateParameters(@NotNull BuildStartContext context) {
+    addParameterIfNotExist(LeechSettings.DOWNLOAD_ENABLED, myConfigurator.isAgentDownloadingEnabled(), context);
+    addParameterIfNotExist(SeedSettings.SEEDING_ENABLED, myConfigurator.isAgentSeedingEnabled(), context);
+  }
+
+  private void addParameterIfNotExist(String name, boolean value, BuildStartContext context) {
+    if (!context.getSharedParameters().containsKey(name)) {
+      context.addSharedParameter(name, String.valueOf(value));
+    }
+  }
+}
